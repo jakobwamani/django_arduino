fb8e9b8c59c985d28c99e38fb74dee9f9e7103a1
Wamani Jacob <nicobwan@gmail.com>
2021-07-07 10:17:23 +0000

    Add_info

--- vivian/vivian_starts.sh
+++ vivian/vivian_starts.sh
@@ -4,12 +4,20 @@
 source /home/ubuntu/.venvs/tangibleai/bin/activate
 echo "I have reached the environment.........."
 #get Vivian to start collecting the data
-/home/ubuntu/.venvs/tangibleai/bin/python  /home/ubuntu/.code/django_arduino/vivian/insert_data_to_database.py
-echo "Collecting data now..............."
 
 #Vivian gets the csv file
-/home/ubuntu/.venvs/tangibleai/bin/python  /home/ubuntu/.code/django_arduino/vivian/insert_data_to_database.py
+/home/ubuntu/.venvs/tangibleai/bin/python  /home/ubuntu/.code/django_arduino/vivian/get_database_info_to_csv.py
 echo "making a csv............"
 
 
+#Vivian saves thats data to heroku git
+/usr/bin/git  status
 
+#Vivian adds that status to git
+/usr/bin/git add .
+
+#Vivian commits that status
+/usr/bin/git commit -am "Add_info"
+
+#Vivian pushes all that to heroku
+/usr/bin/git push heroku master
